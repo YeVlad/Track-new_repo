aa65b1c0afd2bd357ae506d54005d964474a1da3
YeVlad <150618209+YeVlad@users.noreply.github.com>
2024-10-14 22:00:06 +0200

    6

--- src/components/FiltersForm/FiltersForm.jsx
+++ src/components/FiltersForm/FiltersForm.jsx
@@ -41,12 +41,7 @@ const FiltersForm = () => {
           </label>
           <div className={css.location_div}>
             <svg className={[css.icon, css.map_field_icon].join(" ")}>
-              {/* <use xlinkHref="../sprite/sprite.svg#icon-map"></use>{" "} */}
-              <use
-                href={`${
-                  new URL("../../assets/icons/sprite.svg", import.meta.url).href
-                }#icon-map`}
-              ></use>
+              <use href="../../assets/sprite.svg#icon-map"></use>
             </svg>
 
             <Field className={css.location_field} name="location" />
