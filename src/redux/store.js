729458a4bbeeb45533d1ff0be225f9b11a434ee3
YeVlad <150618209+YeVlad@users.noreply.github.com>
2024-10-15 09:13:15 +0200

    renove persist error

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,26 +1,21 @@
-// import { configureStore } from "@reduxjs/toolkit";
-// import { filtersReducer } from "./filters/slice.js";
-// import { likesReducer } from "./likes/slice.js";
-// import { tracksReducer } from "./tracks/slice.js";
-
-// export const store = configureStore({
-//   reducer: {
-//     tracks: tracksReducer,
-//     filters: filtersReducer,
-//     likes: likesReducer,
-//   },
-// });
-
 import { configureStore } from "@reduxjs/toolkit";
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
 import { filtersReducer } from "./filters/slice.js";
 import { likesReducer } from "./likes/slice.js";
 import { tracksReducer } from "./tracks/slice.js";
 
-import { persistStore, persistReducer } from 'redux-persist';
-import storage from 'redux-persist/lib/storage'; 
-
 const likesPersistConfig = {
-  key: 'likes',
+  key: "likes",
   storage,
 };
 
@@ -30,8 +25,14 @@ export const store = configureStore({
   reducer: {
     tracks: tracksReducer,
     filters: filtersReducer,
-    likes: persistedLikesReducer, 
+    likes: persistedLikesReducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
 
 export const persistor = persistStore(store);
